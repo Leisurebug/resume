0c261f70f700cb1380ac3eed7d22fda9038a6bce
Akanksha <121138804+Leisurebug@users.noreply.github.com>
2024-12-18 16:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".nav-link").on("click", function (e) {
+        e.preventDefault();
+        const target = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(target).offset().top - 50
+        }, 1000);
+    });
+});
